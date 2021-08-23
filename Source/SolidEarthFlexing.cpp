4ce09d1849ea87c118195d18512e78b6c1a9bf09
mpzinke <mathew.zinke@hotmail.com>
2021-08-23 17:56:59 -0500

    Updates SolidEarthFlexing.cpp to have new, cool codeblock comment style

--- Source/SolidEarthFlexing.cpp
+++ Source/SolidEarthFlexing.cpp
@@ -41,28 +41,29 @@ TEMP get_number_from_cin(string message, TEMP min, TEMP max)
 // get user input for date, time
 GeoLocation create_geolocation_from_user_input()
 {
-	// LN27–48
-	// *** query section
-	// 
-	//     1 write(*,'(a$)') 'Enter year    [1901-2099]: '
-	//       read(*,*) iyr
-	//       if(iyr.lt.1901.or.iyr.gt.2099) go to 1
-	// 
-	//     2 write(*,'(a$)') 'Enter month number [1-12]: '
-	//       read(*,*) imo
-	//       if(imo.lt.1.or.imo.gt.12) go to 2
-	// 
-	//     3 write(*,'(a$)') 'Enter day          [1-31]: '
-	//       read(*,*) idy
-	//       if(idy.lt.1.or.idy.gt.31) go to 3
-	// 
-	//     4 write(*,'(a$)') 'Lat. (pos N.) [- 90, +90]: '
-	//       read(*,*) glad
-	//       if(glad.lt.-90.d0.or.glad.gt.90.d0) go to 4
-	// 
-	//     5 write(*,'(a$)') 'Lon. (pos E.) [-360,+360]: '
-	//       read(*,*) glod
-	//       if(glod.lt.-360.d0.or.glod.gt.360.d0) go to 5
+	/* LN27–48
+	|*** query section
+	|
+	|    1 write(*,'(a$)') 'Enter year    [1901-2099]: '
+	|      read(*,*) iyr
+	|      if(iyr.lt.1901.or.iyr.gt.2099) go to 1
+	|
+	|    2 write(*,'(a$)') 'Enter month number [1-12]: '
+	|      read(*,*) imo
+	|      if(imo.lt.1.or.imo.gt.12) go to 2
+	|
+	|    3 write(*,'(a$)') 'Enter day          [1-31]: '
+	|      read(*,*) idy
+	|      if(idy.lt.1.or.idy.gt.31) go to 3
+	|
+	|    4 write(*,'(a$)') 'Lat. (pos N.) [- 90, +90]: '
+	|      read(*,*) glad
+	|      if(glad.lt.-90.d0.or.glad.gt.90.d0) go to 4
+	|
+	|    5 write(*,'(a$)') 'Lon. (pos E.) [-360,+360]: '
+	|      read(*,*) glod
+	|      if(glod.lt.-360.d0.or.glod.gt.360.d0) go to 5
+	*/
 	double latitude = get_number_from_cin("Latitude [-90, +90] °N: ", -90.0, 90.0);
 	double longitude = get_number_from_cin("Longitude [-180, +180] °E: ", -180.0, 180.0);
 
